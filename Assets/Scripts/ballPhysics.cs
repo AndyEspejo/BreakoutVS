2c3fd2dae440427071416e7e1f1e280fccedcff4
Andy Espejo <andy.espejo5@gmail.com>
2017-09-09 17:26:54 -0700

    Added initial paddle and basic ball movement(left and right)

---
+++ Assets/Scripts/ballPhysics.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballPhysics : MonoBehaviour {
+
+    public float initForce;
+
+    enum Direction { left, right};
+    private Rigidbody2D ballBody;
+
+	// Use this for initialization
+	void Start () {
+        ballBody = this.gameObject.GetComponent<Rigidbody2D>();
+        Direction ballDirection = Random.value < .5 ? Direction.left : Direction.right;
+        initMovement(ballDirection);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void initMovement(Direction ballDirection) {
+        if (ballDirection == Direction.left) {
+            ballBody.AddForce(Vector3.left * initForce);
+        } else if(ballDirection == Direction.right) {
+            ballBody.AddForce(Vector3.left * initForce);
+        }
+    }
+}
---
+++ Assets/Scripts/movement2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement2 : MonoBehaviour {
+    private Rigidbody2D paddle;
+
+    public float force;
+
+    void Start() {
+        paddle = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Update() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            paddle.AddForce(Vector3.up * force);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow)) {
+            paddle.AddForce(Vector3.down * force);
+        }
+    }
+}
