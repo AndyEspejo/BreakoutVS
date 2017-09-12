16472d88b06a9011c63e718f03d11c2b074407f4
Andy Espejo <andy.espejo5@gmail.com>
2017-09-12 16:43:00 -0700

    Simplified some code in ballPhysics

--- Assets/Scripts/ballPhysics.cs
+++ Assets/Scripts/ballPhysics.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Implements basic physics for a pong ball
+// The so far simple physic calculations are from a post on here: https://www.gamedev.net/forums/topic/396753-pong-physics/
 public class ballPhysics : MonoBehaviour {
 
     public float initForce;
-    Direction ballDirection;
-
-    enum Direction { left, right};
+    public Direction ballDirection;
+    public enum Direction { left, right};
 
     private Rigidbody2D ballBody;
     private GameObject hitPaddle;
@@ -15,16 +17,22 @@ public class ballPhysics : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         ballBody = this.gameObject.GetComponent<Rigidbody2D>();
+
+        // Start the ball going in a random direction
         ballDirection = Random.value < .5 ? Direction.left : Direction.right;
         initMovement(ballDirection);
     }
 	
 	// Update is called once per frame
 	void Update () {
+        // Just constantly makes the ball go faster for now
+        // Adding conditions for when it changes later
         initForce++;
     }
 
+    // Only used for the initial force on the ball
     void initMovement(Direction ballDirection) {
+        
         if (ballDirection == Direction.left) {
             ballBody.AddForce(Vector3.left * initForce);
         }
@@ -33,73 +41,67 @@ public class ballPhysics : MonoBehaviour {
         } 
     }
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        
-        if(collision.gameObject.tag == "Paddles" ) {
+    void OnCollisionEnter2D(Collision2D collision) { 
+        if(collision.gameObject.tag == "Paddles" || collision.gameObject.tag == "Wall") {
             calculateBounce(collision.gameObject);
         }
-
-        //eventually changing 3 to its own, changing variable
-        if (collision.gameObject.tag == "Wall") {
-            calculateBounce(collision.gameObject);     
+        if (collision.gameObject.tag == "Brick") {
+            // TODO
+            // Will need to calculate bounce as well as destroy brick/add point/get power ups, etc   
         }
-
-
     }
 
-    void calculateBounce(GameObject paddle) {
-        if (paddle.tag == "Paddles") {
-            paddleLogic.paddleDir hitPaddleDir = paddle.GetComponent<paddleLogic>().dir;
+    void calculateBounce(GameObject objectHit) {
+        // If the ball has collided with a paddle:
+        if (objectHit.tag == "Paddles") {
+            paddleLogic.paddleDir hitPaddleDir = objectHit.GetComponent<paddleLogic>().dir;
+            // Check if paddle hit is moving or not, as well as which paddle it hit
+            // If the paddle is moving, we add "spin" to it in the same vertical direction and 
+            // then reverse the horizontal direction, else just reverse the horizontal direction
+
             if (hitPaddleDir == paddleLogic.paddleDir.up) {
-                if (paddle.name == "paddle") {
+                // Left paddle hit, going up, so add force up and to the right, etc
+                if (objectHit.name == "paddle") {
                     ballBody.AddForce(Vector3.right * initForce);
                     ballBody.AddForce(Vector3.up * initForce);
-                }
-                if (paddle.name == "paddle2") {
+                } if (objectHit.name == "paddle2") {
                     ballBody.AddForce(Vector3.left * initForce);
                     ballBody.AddForce(Vector3.up * initForce);
                 }
-            }
-            else if (hitPaddleDir == paddleLogic.paddleDir.down) {
-                if (paddle.name == "paddle") {
+            } else if (hitPaddleDir == paddleLogic.paddleDir.down) {
+                if (objectHit.name == "paddle") {
                     ballBody.AddForce(Vector3.right * initForce);
                     ballBody.AddForce(Vector3.down * initForce);
                 }
-                if (paddle.name == "paddle2") {
+                if (objectHit.name == "paddle2") {
                     ballBody.AddForce(Vector3.left * initForce);
                     ballBody.AddForce(Vector3.down * initForce);
                 }
-            }
-            else {
-                if (paddle.name == "paddle") {
+            } else {
+                if (objectHit.name == "paddle") {
                     ballBody.AddForce(Vector3.right * initForce);
                 }
-                if (paddle.name == "paddle2") {
+                if (objectHit.name == "paddle2") {
                     ballBody.AddForce(Vector3.left * initForce);
                 }
             }
         }
-        
-        
-        //Hit a wall
-        if (paddle.name == "topWall") {
-            if(ballBody.velocity.x > 0) {
+
+        // If the ball hit a wall
+        if(objectHit.tag == "Wall") {
+            // Make the horizontal direction = to current direction
+            if (ballBody.velocity.x > 0) {
                 ballBody.AddForce(Vector3.right * initForce);
             } else {
                 ballBody.AddForce(Vector3.left * initForce);
             }
-            ballBody.AddForce(Vector3.down * initForce);
 
-        }
-        if (paddle.name == "botWall") {
-            if (ballBody.velocity.x > 0) {
-                ballBody.AddForce(Vector3.right * initForce);
+            // Then change the vertical direction to be opposite the wall it hit
+            if (objectHit.name == "topWall") {
+                ballBody.AddForce(Vector3.down * initForce);
+            } else {
+                ballBody.AddForce(Vector3.up * initForce);
             }
-            else {
-                ballBody.AddForce(Vector3.left * initForce);
-            }
-            ballBody.AddForce(Vector3.up * initForce);
-
         }
 
     }
--- Assets/Scripts/paddleLogic.cs
+++ Assets/Scripts/paddleLogic.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class paddleLogic : MonoBehaviour {
 
     private Rigidbody2D paddleBody;
+
+
     public Vector2 force;
     public paddleDir dir;
     public enum paddleDir { up, down, still}
@@ -13,18 +15,22 @@ public class paddleLogic : MonoBehaviour {
         paddleBody = this.gameObject.GetComponent<Rigidbody2D>();
     }
     void Update() {
+        // Resets the velocity so paddles don't slide
         paddleBody.velocity = Vector3.zero;
 
+        // Simple paddle controls:
+        // Player 1(left) uses W and S
+        // Player 2(right) uses Up Arrow and Down Arrow
+
+        //The dir variable of each paddle is used in ballPhysics.cs to determine spin on ball
         if (this.gameObject.name == "paddle") {
             if (Input.GetKey(KeyCode.W)) {
                 dir = paddleDir.up;
                 paddleBody.position = paddleBody.position + force;
-            }
-            else if (Input.GetKey(KeyCode.S)) {
+            } else if (Input.GetKey(KeyCode.S)) {
                 dir = paddleDir.down;
                 paddleBody.position = paddleBody.position - force;
-            }
-            else {
+            } else {
                 dir = paddleDir.still;
             }
         }
@@ -33,12 +39,10 @@ public class paddleLogic : MonoBehaviour {
             if (Input.GetKey(KeyCode.UpArrow)) {
                 dir = paddleDir.up;
                 paddleBody.position = paddleBody.position + force;
-            }
-            else if (Input.GetKey(KeyCode.DownArrow)) {
+            } else if (Input.GetKey(KeyCode.DownArrow)) {
                 dir = paddleDir.down;
                 paddleBody.position = paddleBody.position - force;
-            }
-            else {
+            } else {
                 dir = paddleDir.still;
             }
         }
